b9a045b8550b5b37aa8a2ee80695367b86f0ffc9
surajthomask <suthomas@altayer.com>
2018-08-29 15:25:47 +0400

    Initial Commit
    Change-Id: I3bf20f1cc93ef26d4e9834ce1167cebc133149c9

---
+++ ATGValidator/ATGValidator.h
@@ -0,0 +1,22 @@
+//
+//  ATGValidator.h
+//  ATGValidator
+//
+//  Created by Suraj Thomas K on 8/29/18.
+//  Copyright © 2018 Al Tayer Group LLC. All rights reserved.
+//
+//  Save to the extent permitted by law, you may not use, copy, modify,
+//  distribute or create derivative works of this material or any part
+//  of it without the prior written consent of Al Tayer Group LLC.
+//  Any reproduction of this material must contain this notice.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATGValidator.
+FOUNDATION_EXPORT double ATGValidatorVersionNumber;
+
+//! Project version string for ATGValidator.
+FOUNDATION_EXPORT const unsigned char ATGValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATGValidator/PublicHeader.h>
---
+++ ATGValidatorTests/ATGValidatorTests.swift
@@ -0,0 +1,40 @@
+//
+//  ATGValidatorTests.swift
+//  ATGValidatorTests
+//
+//  Created by Suraj Thomas K on 8/29/18.
+//  Copyright © 2018 Al Tayer Group LLC. All rights reserved.
+//
+//  Save to the extent permitted by law, you may not use, copy, modify,
+//  distribute or create derivative works of this material or any part
+//  of it without the prior written consent of Al Tayer Group LLC.
+//  Any reproduction of this material must contain this notice.
+//
+
+import XCTest
+@testable import ATGValidator
+
+class ATGValidatorTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
