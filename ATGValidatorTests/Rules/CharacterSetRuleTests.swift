9f9f4b3184f243c907fc032b8d96af4d73e5ca7f
surajthomask <suthomas@altayer.com>
2018-09-17 12:33:26 +0400

    Add character set rule unit tests
    Change-Id: I8de8570ac705677dfe200ebb62ec02a43beb08fc

---
+++ ATGValidatorTests/Rules/CharacterSetRuleTests.swift
@@ -0,0 +1,294 @@
+//
+//  CharacterSetRuleTests.swift
+//  ATGValidatorTests
+//
+//  Created by Suraj Thomas K on 9/16/18.
+//  Copyright © 2018 Al Tayer Group LLC. All rights reserved.
+//
+//  Save to the extent permitted by law, you may not use, copy, modify,
+//  distribute or create derivative works of this material or any part
+//  of it without the prior written consent of Al Tayer Group LLC.
+//  Any reproduction of this material must contain this notice.
+//
+
+import XCTest
+@testable import ATGValidator
+
+class CharacterSetRuleTests: XCTestCase {
+
+    func testInvalidType() {
+
+        let testValue = 34
+        let characterSet = CharacterSet.alphanumerics
+
+        let rule = CharacterSetRule(characterSet: characterSet)
+        let result = rule.validate(value: testValue)
+
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.invalidType])
+        XCTAssertEqual(result.value as? Int, testValue)
+    }
+
+    func testContains() {
+
+        let characterSet = CharacterSet.uppercaseLetters
+        let rule = CharacterSetRule(characterSet: characterSet, mode: .contains(min: 5, max: 7))
+
+        var testValue = "A String With 6 UpperCase Letters.!"
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "A string with 1 uppercase letter.!"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.characterSetError])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "A STRING FULL OF UPPERCASE LETTERS.!"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.characterSetError])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.characterSetError])
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+
+    func testContainsOnly() {
+
+        let characterSet = CharacterSet.lowercaseLetters
+        let rule = CharacterSetRule(characterSet: characterSet, mode: .containsOnly)
+
+        var testValue = "lowercase"
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "camelCase!"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.characterSetError])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+
+    func testDoesNotContain() {
+
+        let characterSet = CharacterSet.symbols
+        let rule = CharacterSetRule(characterSet: characterSet, mode: .doesNotContain)
+
+        var testValue = "A String With No Symbols."
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "Is $ a symbol?"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.characterSetError])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+
+    func testContainsNumber() {
+
+        let rule = CharacterSetRule.containsNumber(minimum: 1, maximum: 3)
+
+        var testValue = "Hey there.! 1 is good enough."
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "zero numbers are not enough"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "Maximum of 3 numbers are allowed. So what to do with 999 other numbers."
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+
+    func testContainsUpperCase() {
+
+        let rule = CharacterSetRule.containsUpperCase(minimum: 1, maximum: 3)
+
+        var testValue = "Hey there.! 1 upper case is good enough."
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "zero upper case are not enough"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "Maximum Of 3 UpperCases Are Allowed."
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+
+    func testContainsLowerCase() {
+
+        let rule = CharacterSetRule.containsLowerCase(minimum: 1, maximum: 3)
+
+        var testValue = "HEY THERE, 'a' IS THE ONLY LOWER CASE HERE."
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "0 LOWER CASES ARE NOT ENOUGH."
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "Maximum Of 3 LowerCases Are Allowed."
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+
+    func testContainsSymbols() {
+
+        let rule = CharacterSetRule.containsSymbols(minimum: 1, maximum: 3)
+
+        var testValue = "$ is the only symbol HERE"
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "0 SYMBOLS ARE NOT ENOUGH"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "$Maximum Of ^3^ symbols are allowed$"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.occurrencesNotInRange])
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+
+    func testNumbersOnly() {
+
+        let rule = CharacterSetRule.numbersOnly
+
+        var testValue = "0555898666"
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "0000o0000"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.invalidType])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+
+    func testLowerCaseOnly() {
+
+        let rule = CharacterSetRule.lowerCaseOnly
+
+        var testValue = "lowercase"
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "lowerCase"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.invalidType])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+
+    func testUpperCaseOnly() {
+
+        let rule = CharacterSetRule.upperCaseOnly
+
+        var testValue = "UPPERCASE"
+        var result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = "UpperCase"
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .failure)
+        XCTAssertEqual(result.errors as? [ValidationError], [ValidationError.invalidType])
+        XCTAssertEqual(result.value as? String, testValue)
+
+        testValue = ""
+        result = rule.validate(value: testValue)
+        XCTAssertEqual(result.status, .success)
+        XCTAssertNil(result.errors)
+        XCTAssertEqual(result.value as? String, testValue)
+    }
+}
